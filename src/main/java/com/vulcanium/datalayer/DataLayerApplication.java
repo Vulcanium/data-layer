33c8001f11421c00770f5cc50aac8c570e2111a9
DESKTOP-O7M6SM5\trubl <trublereau@msn.com>
2026-04-10 15:49:51 +0200

    Indent files

--- src/main/java/com/vulcanium/datalayer/DataLayerApplication.java
+++ src/main/java/com/vulcanium/datalayer/DataLayerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DataLayerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataLayerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataLayerApplication.class, args);
+    }
 
 }
